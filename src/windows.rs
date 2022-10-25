415c5c89bc590ff1ba5ce99655a7001f475c09c0
Joe Richey <joerichey@google.com>
2022-10-24 23:04:08 -0700

    Add status_to_result helper function
    Signed-off-by: Joe Richey <joerichey@google.com>

--- src/windows.rs
+++ src/windows.rs
@@ -33,21 +33,25 @@ fn bcrypt_random(s: &mut [MaybeUninit<u8>]) -> NTSTATUS {
     unsafe { BCryptGenRandom(ptr::null_mut(), ptr, len, BCRYPT_USE_SYSTEM_PREFERRED_RNG) }
 }
 
+fn status_to_result(ret: NTSTATUS) -> Result<(), Error> {
+    // NTSTATUS codes use the two highest bits for severity status.
+    if ret >> 30 != 0b11 {
+        return Ok(());
+    }
+    // We zeroize the highest bit, so the error code will reside
+    // inside the range designated for OS codes.
+    let code = ret ^ (1 << 31);
+    // SAFETY: the second highest bit is always equal to one,
+    // so it's impossible to get zero. Unfortunately the type
+    // system does not have a way to express this yet.
+    let code = unsafe { NonZeroU32::new_unchecked(code) };
+    Err(Error::from(code))
+}
+
 pub fn getrandom_inner(dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
     // Prevent overflow of u32
     for chunk in dest.chunks_mut(u32::max_value() as usize) {
-        let ret = bcrypt_random(chunk);
-        // NTSTATUS codes use the two highest bits for severity status.
-        if ret >> 30 == 0b11 {
-            // We zeroize the highest bit, so the error code will reside
-            // inside the range designated for OS codes.
-            let code = ret ^ (1 << 31);
-            // SAFETY: the second highest bit is always equal to one,
-            // so it's impossible to get zero. Unfortunately the type
-            // system does not have a way to express this yet.
-            let code = unsafe { NonZeroU32::new_unchecked(code) };
-            return Err(Error::from(code));
-        }
+        status_to_result(bcrypt_random(chunk))?;
     }
     Ok(())
 }
